3a8fef449eda2909431eb4bfc741221b0aa895fa
Alexander Sparkowsky <info@roamingthings.de>
2017-05-01 11:34:30 +0200

    Migrated from maven to gradle

--- rules-jackson/src/test/java/de/roamingthings/junit/rules/jackson/JacksonObjectMapperProviderTest.java
+++ rules-jackson/src/test/java/de/roamingthings/junit/rules/jackson/JacksonObjectMapperProviderTest.java
@@ -5,10 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-/**
- * @author alxs
- * @version 2016/06/22
- */
 public class JacksonObjectMapperProviderTest {
     @Rule
     public JacksonObjectMapperProvider provider = new JacksonObjectMapperProvider();
