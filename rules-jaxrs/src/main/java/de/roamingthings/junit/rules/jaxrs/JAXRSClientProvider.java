e1a36367d5d885baaddb63a16d090c1657082f1d
Alexander Sparkowsky <info@roamingthings.de>
2017-05-01 11:25:55 +0200

    Migrated from maven to gradle

--- rules-jaxrs/src/main/java/de/roamingthings/junit/rules/jaxrs/JAXRSClientProvider.java
+++ rules-jaxrs/src/main/java/de/roamingthings/junit/rules/jaxrs/JAXRSClientProvider.java
@@ -1,24 +1,20 @@
-package de.roamingthings.junit.rules.jaxrs;
-
-/*
- * #%L
- * rules-jpa
- * %%
- * Copyright (C) 2016 Alexander Sparkowsky
- * %%
+/**
+ * Copyright 2016 Alexander Sparkowsky
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
+package de.roamingthings.junit.rules.jaxrs;
+
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -27,9 +23,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-/**
- *
- */
 public class JAXRSClientProvider implements TestRule {
 
     private Client client;
--- rules-jaxrs/src/test/java/de/roamingthings/junit/rules/jaxrs/JAXRSClientProviderTest.java
+++ rules-jaxrs/src/test/java/de/roamingthings/junit/rules/jaxrs/JAXRSClientProviderTest.java
@@ -1,42 +1,22 @@
 package de.roamingthings.junit.rules.jaxrs;
 
-/*
- * #%L
- * rules-jaxrs
- * %%
- * Copyright (C) 2016 Alexander Sparkowsky
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-import static de.roamingthings.junit.rules.jaxrs.HttpMatchers.successful;
+import org.junit.Rule;
+import org.junit.Test;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import static de.roamingthings.junit.rules.jaxrs.HttpMatchers.successful;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-import org.junit.Rule;
-import org.junit.Test;
 
-/**
- *
- */
 public class JAXRSClientProviderTest {
 
     @Rule
-    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://www.java.com");
+    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("https://www.java.com");
 
     @Test
     public void pingJava() {
@@ -45,7 +25,7 @@ public class JAXRSClientProviderTest {
         WebTarget target = provider.target();
         assertNotNull(target);
         assertNotNull(provider.addPath("en"));
-        assertNotNull(provider.target("http://www.google.de"));
+        assertNotNull(provider.target("https://www.google.de"));
 
         Response response = target.request(MediaType.TEXT_HTML).get();
         assertThat(response.getStatus(), is(200));
@@ -58,7 +38,7 @@ public class JAXRSClientProviderTest {
         WebTarget target = provider.target();
         assertNotNull(target);
         assertNotNull(provider.addPath("en"));
-        assertNotNull(provider.target("http://www.google.de"));
+        assertNotNull(provider.target("https://www.google.de"));
 
         Response response = target.request(MediaType.TEXT_HTML).get();
         assertThat(response, is(successful()));
