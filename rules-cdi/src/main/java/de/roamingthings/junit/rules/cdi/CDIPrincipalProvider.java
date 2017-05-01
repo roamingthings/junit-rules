1fa52c224f530027cce98ff4e4824b782d7316a5
Alexander Sparkowsky <info@roamingthings.de>
2017-05-01 10:52:40 +0200

    Switched rules-cdi from maven to gradle

--- rules-cdi/src/main/java/de/roamingthings/junit/rules/cdi/CDIPrincipalProvider.java
+++ rules-cdi/src/main/java/de/roamingthings/junit/rules/cdi/CDIPrincipalProvider.java
@@ -1,24 +1,19 @@
-package de.roamingthings.junit.rules.cdi;
-
-/*
- * #%L
- * rules-cdi
- * %%
- * Copyright (C) 2016 Alexander Sparkowsky
- * %%
+/**
+ * Copyright 2016 Alexander Sparkowsky
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
+package de.roamingthings.junit.rules.cdi;
 
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -32,10 +27,6 @@ import java.security.Principal;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * @author alxs
- * @version 2016/03/24
- */
 public class CDIPrincipalProvider implements TestRule {
 
     public static final String TEST_PRINCIPAL_NAME = "testPrincipalName";
--- rules-cdi/src/test/java/de/roamingthings/junit/rules/cdi/CDIPrincipalProviderTest.java
+++ rules-cdi/src/test/java/de/roamingthings/junit/rules/cdi/CDIPrincipalProviderTest.java
@@ -1,25 +1,5 @@
 package de.roamingthings.junit.rules.cdi;
 
-/*
- * #%L
- * rules-cdi
- * %%
- * Copyright (C) 2016 Alexander Sparkowsky
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -33,10 +13,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
-/**
- * @author alxs
- * @version 2016/03/24
- */
 public class CDIPrincipalProviderTest {
     @Rule
     public CDIPrincipalProvider principalProvider = new CDIPrincipalProvider();
